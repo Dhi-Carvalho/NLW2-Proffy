20ac84934628d1200af1129ea749f647e5241d6b
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-03-03 21:02:41 -0300

    added to post route

--- src/server.js
+++ src/server.js
@@ -1,78 +1,19 @@
-const proffys = [
-    {
-        name: "Diego Fernades",
-        avatar: "https://avatars.githubusercontent.com/u/2254731?s=460&u=4fcc8ca9672eeb41ea800271831b7c687bc17054&v=4",
-        whatsapp: 85452233464,
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação Física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química"
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado"
-]
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req,res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-    const isNotEmpty = Object.keys(data).length != 0
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
 
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
+
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
 
 server
+.use(express.urlencoded({extended: true}))
 .use(express.static("public"))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 .listen(5500)
